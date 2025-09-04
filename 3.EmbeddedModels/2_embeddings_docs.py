cb68e565713f12dcfe194875d1f309a16a4c3414
Swapnil-SM <swapnilmishra413@gmail.com>
2025-09-05 02:27:36 +0530

    final commit

---
+++ 3.EmbeddedModels/2_embeddings_docs.py
@@ -0,0 +1,18 @@
+# for multiple line texts documents embedding
+
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+result = embedding.embed_documents(documents)   #for multi line query we use embed_documents
+
+print(str(result))
